9ffa8772c9497933842ef9efcce37181d0cf08a6
Linkbreathe <linking626@gmail.com>
2024-05-18 18:15:20 +0800

    Build the Frame Finish the basic Components and using styled components to build it

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Sidebar from './Sidebar';
+import Header from './Header';
+import { Outlet } from 'react-router-dom';
+import styled from 'styled-components';
+const StyledAppLayout = styled.div`
+    display: grid;
+    grid-template-columns: 26rem 3fr;
+    grid-template-rows: auto 1fr;
+    height:100vh;
+`
+const Main = styled.main`
+    background-color:var(--color-grey-50);
+    padding:4rem 4.8rem 6.4rem;
+`
+function AppLayout() {
+  return (
+    <StyledAppLayout>
+        <Sidebar />
+        <Header />
+        <Main>
+            <Outlet />
+        </Main>
+    </StyledAppLayout>
+  )
+}
+
+export default AppLayout;
